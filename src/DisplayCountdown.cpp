db111a245291fb7d2229a9f9589891a9d696e18a
Niclas Nilsson <thenajnth@gmail.com>
2022-06-07 20:46:03 +0200

    FORMATTING: Fixed some strange indentation

--- src/DisplayCountdown.cpp
+++ src/DisplayCountdown.cpp
@@ -1,33 +1,33 @@
-    #include "DisplayCountdown.h"
+#include "DisplayCountdown.h"
     
-    /**
-     * Sets pointer to display object to use.
-     */
-    void DisplayCountdown::set_display(MD_Parola *display) {
-        this->display = display;
-    }
+/**
+* Sets pointer to display object to use.
+*/
+void DisplayCountdown::set_display(MD_Parola *display) {
+    this->display = display;
+}
 
-    /**
-     * Set module in run mode (to actually run, the tick()-method
-     * has to be called every n milliseconds.)
-     */
-    void DisplayCountdown::run(int start_no) {
+/**
+ * Set module in run mode (to actually run, the tick()-method
+ * has to be called every n milliseconds.)
+ */
+void DisplayCountdown::run(int start_no) {
+    last_decrement = millis();
+    current_no = start_no;
+}
+
+/**
+ * Move the state of the module forward (if it should by this tick).
+ */
+ bool DisplayCountdown::tick() {
+    if ((millis() - last_decrement) >= 1000) {
         last_decrement = millis();
-        current_no = start_no;
+        current_no--;
+
+        display->setTextAlignment(PA_RIGHT);
+        display->printf("%2d s", current_no);
+
+        return (current_no == 0); // If 0, returns true
     }
-
-    /**
-    * Move the state of the module forward (if it should by this tick).
-    */
-    bool DisplayCountdown::tick() {
-        if ((millis() - last_decrement) >= 1000) {
-            last_decrement = millis();
-            current_no--;
-
-            display->setTextAlignment(PA_RIGHT);
-            display->printf("%2d s", current_no);
-
-            return (current_no == 0); // If 0, returns true
-        }
-        return false;
-    }
+    return false;
+}
